be94d212cf79319f236970b3fe8c1733898892f3
Nalinsrvstv <118289022+Nalinsrvstv@users.noreply.github.com>
2024-11-05 13:54:15 +0530

    Update settings_dev.py

--- busSchedulingServer/settings_dev.py
+++ busSchedulingServer/settings_dev.py
@@ -10,7 +10,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "test_base2",#"ebus",  # Replace with your database name
-        "USER": "test_back";#"rohit",  # Replace with your database user
+        "USER": "test_back",#"rohit",  # Replace with your database user
         "PASSWORD": "1234",#"rohit",  # Replace with your database password
         "HOST": "localhost",  # Replace with your database host if necessary
         "PORT": "5432",
