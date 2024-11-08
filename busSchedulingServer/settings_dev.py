d9b69c249917a2a1dc9f5741e7b735237365a4e3
Nalinsrvstv <118289022+Nalinsrvstv@users.noreply.github.com>
2024-11-09 00:32:52 +0530

    Update settings_dev.py

--- busSchedulingServer/settings_dev.py
+++ busSchedulingServer/settings_dev.py
@@ -12,7 +12,7 @@ DATABASES = {
         "NAME": "test_2",#"ebus",  # Replace with your database name
         "USER": "nalin",#"rohit",  # Replace with your database user
         "PASSWORD": "2209",#"rohit",  # Replace with your database password
-        "HOST": "13.233.11.141",  # Replace with your database host if necessary
+        "HOST": "13.201.132.97",  # Replace with your database host if necessary
         "PORT": "5432",
     }
 }
